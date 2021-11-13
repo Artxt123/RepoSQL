027c8b86feed3e94f529fec579a56813f7f7864d
Artur Oczkowski <alfaix@o2.pl>
2021-11-14 00:00:30 +0100

    SimpleSQLProject

--- SimpleSQLProject/SimpleSQLProject/Program.cs
+++ SimpleSQLProject/SimpleSQLProject/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace SimpleSQLProject
 {
@@ -10,6 +12,33 @@ namespace SimpleSQLProject
     {
         static void Main(string[] args)
         {
+            try
+            {
+                //Połączenie się i otwarcie połączenia:
+                var connectionParams = @"Server=.\MYSQLSERVER1; Database=MojeFinanse; Trusted_Connection=True;";
+
+                using (var connection = new SqlConnection(connectionParams)) //gdy kod opuści blok using to połączenie zostanie automatycznie zamknięte
+                {
+                    connection.Open();
+
+                    //Zapisanie komendy TSQL i wykonanie jej:
+                    var sqlCommand = new SqlCommand("SELECT IDKlienta,Imie,Nazwisko,Inicjaly FROM Klienci", connection);
+
+                    using (var reader = sqlCommand.ExecuteReader()) //metoda ExecuteReader zbuduje nam SqlDataReader
+                    {
+                        while (reader.Read()) //jeżeli jest dostępny następny rekord to wartość jest true
+                        {
+                            Console.WriteLine($"{reader[0],-5} {reader[1],-10} {reader[2],-10} {reader[3],-10}");
+                        }
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine("Problem z uruchomieniem kodu TSQL " + ex.Message);
+            }
         }
     }
 }
